9e049339f6bac6ba8ada5887983037b7af51ceb9
孙寿海 <149130775@qq.com>
2022-04-06 22:17:43 +0800

    增加@vuepress/plugin-pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,5 +62,25 @@ module.exports = {
         editLinks: true,
         // 默认为 "Edit this page"
         editLinkText: '帮助我们改善此页面！'
-    }
+    },
+    head: [
+        ['link', { rel: 'icon', href: '/hero.png' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ],
+    plugins: [[
+        '@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: {
+                message: "New content is available.",
+                buttonText: "Refresh"
+            }
+        }
+    ]]
 }
